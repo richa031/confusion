c8765acf771031feaa100e1bc031e7a3bfc3708c
richa031 <richahp031@gmail.com>
2018-12-14 17:16:05 +0530

    react components

---
+++ src/components/MenuComponents.js
@@ -0,0 +1,65 @@
+//menu of dishes(component)
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap';
+
+class Menu extends Component{//adding menu component
+    constructor(props){//defining constructor
+        super(props);
+
+        this.state = {//properties related to the component that we can make use of
+            selectedDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({selectedDish: dish});
+    }
+
+    renderDish(dish){
+        if(dish!=null){
+            return(
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    }
+
+    render(){//will return what to display on UI
+        const menu = this.props.dishes.map((dish) => {//to use state(dishes) of App component here as props
+            return(
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+            
+        });
+
+        return(
+            <div className="container">
+                <div className="row">
+                    {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Menu;//we can import this wherever we want
