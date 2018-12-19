7574879bf66687473f193a46d9f5bee6e62b879e
richa031 <richahp031@gmail.com>
2018-12-19 16:23:05 +0530

    MainComponent added

--- src/components/MenuComponents.js
+++ src/components/MenuComponents.js
@@ -1,31 +1,20 @@
 //menu of dishes(component)
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardTitle, CardBody} from 'reactstrap';
-import Dishdetail from './DishdetailComponents';
+
 
 class Menu extends Component{//adding menu component
     constructor(props){//defining constructor
         super(props);
-
-        this.state = {//properties related to the component that we can make use of
-            selectedDish: null,
-            selectedDish: {
-                comments: null
-            }
-        }
     }
 
-    onDishSelect(dish) {
-        this.setState({selectedDish: dish
-           
-        });
-    }
+    
 
     render(){//will return what to display on UI
         const menu = this.props.dishes.map((dish) => {//to use state(dishes) of App component here as props
             return(
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -42,7 +31,6 @@ class Menu extends Component{//adding menu component
                     {menu}
                 </div>
                 <div className="row">
-                    <Dishdetail dish={this.state.selectedDish} />
                 </div>
             </div>
         );
