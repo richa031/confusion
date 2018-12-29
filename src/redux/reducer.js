917f256dc27df1232fee9b3f6bba3ed231f5a6c3
richa031 <richahp031@gmail.com>
2018-12-29 22:38:26 +0530

    simpler reducer3,4

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,6 +1,6 @@
 //reducer function to set up all the states
-import { PROMOTIONS } from '../shared/promotions';
-import { LEADERS } from '../shared/leaders';
+
+
 
 export const initialState = {//initial config of states
     dishes: DISHES,
